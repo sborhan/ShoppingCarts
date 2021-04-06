fb7c34d41e319a1da8c8702dfa10044ee3319858
Sam Borhan <samborhan1566@gmail.com>
2021-04-06 10:36:33 -0700

    Add TeExtendedEditorControl_Constructor_Should_Pass() in TeExtendedEditorControlTests.cs in UnitTests.csproj

---
+++ UnitTest/Controls/ExtendedEditorControlTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using ShoppingCarts.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Controls
+{
+    class ExtendedEditorControlTests
+    {
+        ExtendedEditorControl control;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TeExtendedEditorControl_Constructor_Should_Pass()
+        {
+            //Arrange
+            control = new ExtendedEditorControl();
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.IsNotNull(control);
+        }
+
+    }
+}
