3d5b07f6a38bfee5fe96961878bc19a61d6e0097
Sam Borhan <samborhan1566@gmail.com>
2021-04-14 13:44:33 -0700

    Resolve version issue in project

---
+++ UnitTest/Controls/ExtendedListViewTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using NUnit.Framework;
+
+using ShoppingCarts.Controls;
+
+
+namespace UnitTest.Controls
+{
+    [TestFixture]
+    class ExtendedListViewTests
+    {
+        ExtendedListView control;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+       // [Test]
+        public void ExtendedListView_Constructor_Should_Pass()
+        {
+            //Arrange
+            control = new ExtendedListView(ListViewCachingStrategy.RecycleElementAndDataTemplate);
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.IsNotNull(control);
+        }
+
+    }
+}
