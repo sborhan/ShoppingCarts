9e020af3ca913eb251f003fd3d0cecdab542e741
Anh Tran <trananh@seattleu.edu>
2021-04-18 10:11:48 -0700

    Add ItemTest.cs constructor test

---
+++ UnitTest/Model/ItemTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using ShoppingCarts.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UnitTest.Model
+{
+    class ItemModelTests
+    {
+        ItemModelTests control;
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ItemModelTests_Constructor_Should_Pass()
+        {
+            //Arrange
+            control = new ItemModelTests();
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.IsNotNull(control);
+        }
+
+
+    }
+
+}
