726e8a15297708aa340b4661b5a1efd993e49cec
Sam Borhan <samborhan1566@gmail.com>
2021-04-25 13:35:08 -0700

    Add UTs to CartDetsilPageTests.cs

---
+++ UnitTest/Views/CartDetailPageTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using NUnit.Framework;
+
+using ShoppingCarts.Controls;
+using ShoppingCarts;
+using System.Windows.Input;
+using ShoppingCarts.Views;
+
+namespace UnitTest.Controls
+{
+    [TestFixture]
+    class CartDetailPageTests
+    {
+        CartDetailPage detailPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+        }
+
+        [Test]
+        public void CartDetailPage_Constructor_Should_Pass()
+        {
+            //Arrange
+            detailPage = new CartDetailPage();
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.IsNotNull(detailPage);
+        }
+
+        [Test]
+        public void CartDetailPage_OnItemSelected_with_New_ListView_sender_Should_Pass()
+        {
+            //Arrange
+            detailPage = new CartDetailPage();
+
+            //Act
+            detailPage.OnItemSelected(new ListView(), null);
+
+            //Reset
+
+            //Assert
+            Assert.Pass();
+        }
+
+    }
+}
