9cee05e6eb0821b4805bdf0dded1b3c22431fe23
Sam Borhan <samborhan1566@gmail.com>
2021-04-25 13:35:31 -0700

    Add UTs to AddProductPageTests.cs

---
+++ UnitTest/Views/AddProductPageTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using NUnit.Framework;
+
+using ShoppingCarts.Controls;
+using ShoppingCarts;
+using System.Windows.Input;
+using ShoppingCarts.Views;
+
+namespace UnitTest.Controls
+{
+    [TestFixture]
+    class AddProductPageTests
+    {
+        AddProductPage productPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+        }
+
+        [Test]
+        public void AddProductPage_Constructor_Should_Pass()
+        {
+            //Arrange
+            productPage = new AddProductPage();
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.IsNotNull(productPage);
+        }
+
+    }
+}
