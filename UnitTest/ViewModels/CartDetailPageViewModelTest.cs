9b5ad90fc4154ae431e4a1b30a224bba28918e0f
Sam Borhan <samborhan1566@gmail.com>
2021-05-28 15:07:31 -0700

    Add new UT's

---
+++ UnitTest/ViewModels/CartDetailPageViewModelTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using ShoppingCarts.ViewModels;
+using ShoppingCarts.Model;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UnitTest.ViewModels
+{
+    class CartDetailPageViewModelTest
+    {
+        CartDetailPageViewModel control;
+
+        [SetUp]
+        public void Setup()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+
+        }
+
+        [Test]
+        public void CartDetailPageViewModelTests_SetItems_Should_Pass()
+        {
+            //Arrange
+            control = new CartDetailPageViewModel();
+            List<Item> ItemList = new List<Item>();
+            ItemList.Add(new Item() { Name = "Item"});
+            //Act
+            control.SetItems(ItemList);
+            //Reset
+
+            //Assert
+            Assert.IsTrue(control.ItemsInCart);
+        }
+    }
+}
+
